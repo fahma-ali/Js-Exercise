b26d9d2d3f206870ed73c27e4d2ec3af902fdf94
fahma ali <fahmaali432@gmail.com>
2026-02-09 07:40:55 +0300

    exercise 1

---
+++ exercise1/main.js
@@ -0,0 +1,15 @@
+let x = 7;
+let y = 3;
+
+console.log("Addition (x + y):", x + y);
+console.log("Subtraction (x - y):", x - y);
+console.log("Multiplication (x * y):", x * y);
+console.log("Division (x / y):", x / y);
+console.log("Remainder (x % y):", x % y);
+
+console.log("Post-increment (x++):", x++);
+console.log("Value of x after post increment:", x);
+console.log("Pre-increment (++x):", ++x);
+console.log("Post-decrement (x--):", x--);
+console.log("Value of x after post decrement:", x);
+console.log("pre-decrement (--x):", --x);
