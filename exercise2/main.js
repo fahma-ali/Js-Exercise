b74f0d5c1204da4c03fb6587ac2a13f4337da9f0
fahma ali <fahmaali432@gmail.com>
2026-02-09 17:01:03 +0300

    exercise 2

---
+++ exercise2/main.js
@@ -0,0 +1,12 @@
+
+
+let a = 15;
+let b ="15";
+console.log( "a is greater then b" ,a > b)
+console.log("a is less then b" , a < b)
+console.log( "a is greater then or equal b" , a >= b)
+console.log("a is less then or equal b" , a <= b)
+console.log( "a value is equal b value " , a == b)
+console.log( "a value isn't equal b value " , a != b)
+console.log(  "a is equal b type and value" , a === b)
+console.log(  "a isn't equal b type and value" , a !== b)
---
+++ practise/main.js
@@ -0,0 +1,10 @@
+// first js practise 2026-2-9
+
+let a = 10
+let b="10"
+console.log(a>=b)
+console.log(a<=b)
+console.log(a != b)
+console.log(a==b)
+console.log(a===b)
+console.log(a !==b )
