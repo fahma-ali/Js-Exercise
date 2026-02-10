c272dc63e45f55ab11eeb848d3b0b5d60d6fff1e
fahma ali <fahmaali432@gmail.com>
2026-02-10 07:47:24 +0300

    practise js

---
+++ practise/main.js
@@ -0,0 +1,30 @@
+// first js practise 2026-2-9
+
+// comparison operators
+
+// let a = 10
+// let b="10"
+// console.log(a>=b)
+// console.log(a<=b)
+// console.log(a != b)
+// console.log(a==b)
+// console.log(a===b)
+// console.log(a !==b )
+
+// string operators
+
+let firstName="Fahma";
+let lastName="Ali"
+console.log("My Name is " ,firstName + "  "+ lastName)
+
+let greeting ="Good morning"
+// greeting = greeting+" "+firstName+" "+lastName
+greeting += " "+firstName+" "+ lastName
+console.log(greeting);
+
+// function declaration
+
+function greeting() {
+    console.log("hello,world");
+}
+ greeting()
