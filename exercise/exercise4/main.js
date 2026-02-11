7e5f469349b6644363e613609105f8f017d68d60
fahma ali <fahmaali432@gmail.com>
2026-02-11 06:33:46 +0300

    exercise 3 and making all exercise one folder

---
+++ exercise/exercise4/main.js
@@ -0,0 +1,16 @@
+
+
+// function declaration
+
+function add(x,y){
+console.log("Total is : $",x+y)
+}
+add(10,20)
+add(30,20)
+
+// function expression
+const add = function(x,y){
+    console.log("Total Addition is :",x+y);
+}
+add(5,6);
+add(50,40)
--- practise/main.js
+++ practise/main.js
@@ -13,18 +13,48 @@
 
 // string operators
 
-let firstName="Fahma";
-let lastName="Ali"
-console.log("My Name is " ,firstName + "  "+ lastName)
+// let firstName="Fahma";
+// let lastName="Ali"
+// console.log("My Name is " ,firstName + "  "+ lastName)
 
-let greeting ="Good morning"
-// greeting = greeting+" "+firstName+" "+lastName
-greeting += " "+firstName+" "+ lastName
-console.log(greeting);
+// let greeting ="Good morning"
+// // greeting = greeting+" "+firstName+" "+lastName
+// greeting += " "+firstName+" "+ lastName
+// console.log(greeting);
 
 // function declaration
 
-function greeting() {
-    console.log("hello,world");
-}
- greeting()
+// function greeting() {
+//     console.log("hello,world");
+// }
+//  greeting()
+
+// function parameters and argument
+
+// function greet(name){
+//     console.log("hello ,  " +name)
+// }
+// greet("fahma ali")
+
+// function calculateTotal(price,qty){
+//     console.log("Total price is  " +price * qty);
+// }
+// calculateTotal(5,5);
+// calculateTotal(5,6);
+
+// return parameters
+// function add(a,b){
+//     return a+b;
+// }
+// let result= add(10,10)
+// console.log(result/2)
+
+// function expression
+
+// const greet = function(name){
+//     console.log("hello," + name);
+// }
+
+// greet("haawa");
+// greet("nuur");
+// greet("fatma");
