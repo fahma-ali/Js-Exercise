c4c8988d588e1b166324a02be1445adeff12f994
fahma ali <fahmaali432@gmail.com>
2026-02-12 07:43:43 +0300

    exercise 7

---
+++ exercise/exercise7/main.js
@@ -0,0 +1,9 @@
+// creating array store different colors
+
+let colors = ["blue","green","red","white"]
+ 
+console.log("before push " ,colors)
+colors.push("orange")
+console.log("after push",colors)
+colors.shift();
+console.log("after shift",colors)
